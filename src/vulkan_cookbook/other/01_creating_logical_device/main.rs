e7fd29b8e63eed6a22d87a50adfe191973fc68d7
srxqds <srxqds@163.com>
2018-09-09 23:35:44 +0800

    add gfx dependence

--- src/vulkan_cookbook/other/01_creating_logical_device/main.rs
+++ src/vulkan_cookbook/other/01_creating_logical_device/main.rs
@@ -1,4 +1,11 @@
 extern crate winit;
+#[cfg(feature = "dx12")]
+extern crate gfx_backend_dx12 as back;
+#[cfg(feature = "metal")]
+extern crate gfx_backend_metal as back;
+#[cfg(feature = "vulkan")]
+extern crate gfx_backend_vulkan as back;
+extern crate gfx_hal as hal;
 
 fn main() {
     let mut events_loop = winit::EventsLoop::new();
