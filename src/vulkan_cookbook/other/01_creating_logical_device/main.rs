b35f2ec5f5df355e22e50ad46eae6821920d544a
srxqds <srxqds@163.com>
2018-09-10 01:55:43 +0800

    override winit dependency

--- src/vulkan_cookbook/other/01_creating_logical_device/main.rs
+++ src/vulkan_cookbook/other/01_creating_logical_device/main.rs
@@ -7,13 +7,30 @@ extern crate gfx_backend_metal as back;
 extern crate gfx_backend_vulkan as back;
 extern crate gfx_hal as hal;
 
+use hal::format::{AsFormat, ChannelType, Rgba8Srgb as ColorFormat, Swizzle};
+use hal::pass::Subpass;
+use hal::pso::{PipelineStage, ShaderStageFlags};
+use hal::queue::Submission;
+use hal::{
+    buffer, command, format as f, image as i, memory as m, pass, pool, pso, window::Extent2D,
+};
+use hal::{Backbuffer, DescriptorPool, FrameSync, Primitive, SwapchainConfig};
+use hal::{Device, Instance, PhysicalDevice, Surface, Swapchain};
+
+
 fn main() {
     let mut events_loop = winit::EventsLoop::new();
 
-    let _window = winit::WindowBuilder::new()
-        .with_title("01 - Creating Logical Device")
-        .build(&events_loop)
-        .unwrap();
+    let wb = winit::WindowBuilder::new()
+        .with_title("01 - Creating Logical Device");
+
+    let (_window, _instance, mut adapters, mut surface) = {
+        let window = wb.build(&events_loop).unwrap();
+        let instance = back::Instance::create("gfx-rs quad", 1);
+        let surface = instance.create_surface(&window);
+        let adapters = instance.enumerate_adapters();
+        (window, instance, adapters, surface)
+    };
 
     events_loop.run_forever(|event| {
         println!("{:?}", event);
